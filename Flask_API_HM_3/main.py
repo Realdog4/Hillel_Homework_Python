a5bbd4070c1277c146c78d82ffb17d46759e1c8f
Bogdan Tsegichko <sirbony@gmail.com>
2023-06-10 00:11:35 +0300

    added def get_bitcoin_value

--- Flask_API_HM_3/main.py
+++ Flask_API_HM_3/main.py
@@ -1,9 +1,21 @@
 import pandas as pd
-from flask import Flask, request
+from flask import Flask, request, Response
 from faker import Faker
+import requests
+from http import HTTPStatus
 
 app = Flask(__name__)
 fake = Faker()
+
+
+@app.route('/')
+def openPage():
+    return "Hello! Use the link to view information: /generate_students or /bitcoin_rate"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Error, please enter the correct address!", 404
+
 
 @app.route('/generate_students', methods=['GET'])
 def generate_students():
@@ -34,6 +46,37 @@ def save_to_csv(student):
     df = pd.DataFrame(student, index=[0])
     df.to_csv('students.csv', mode='a', index=False, header=not df.empty)
 
+
+
+@app.route('/bitcoin_rate', methods=['GET'])
+def get_bitcoin_value(currency='USD', count=1):
+    url = 'https://bitpay.com/rates'
+    response = requests.get(url, {})
+
+    if response.status_code != HTTPStatus.OK:
+        return Response(
+            "ERROR: Something went wrong",
+            status=response.status_code
+        )
+
+    if response.status_code == HTTPStatus.OK:
+        data = response.json()
+        for currency_data in data['data']:
+            if currency_data['code'] == currency:
+                rate = currency_data['rate']
+                symbol = get_symbol(currency)
+                total = rate * count
+                return f"The Bitcoin rate for {currency} is: {total} {symbol}"
+
+def get_symbol(currency):
+    url =  'https://bitpay.com/currencies'
+    response = requests.get(url, {})
+    if response.status_code == HTTPStatus.OK:
+        data = response.json()
+        for currency_data in data['data']:
+            if currency_data['code'] == currency:
+                symbol = currency_data['symbol']
+                return symbol
 
 
 if __name__ == '__main__':
