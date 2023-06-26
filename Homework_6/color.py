85c80d189dbe60f3d522570cc76fe3c8daa01d92
Bogdan Tsegichko <sirbony@gmail.com>
2023-06-26 23:03:29 +0300

    cor. file color and frange

--- Homework_6/color.py
+++ Homework_6/color.py
@@ -10,6 +10,10 @@ class Colorizer:
             print(Fore.RED, end='')
         elif self.color == 'blue':
             print(Fore.BLUE, end='')
+        elif self.color == 'green':
+            print(Fore.GREEN, end='')
+        elif self.color == 'yellow':
+            print(Fore.YELLOW, end='')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         print(Style.RESET_ALL, end='')
@@ -20,6 +24,15 @@ init()
 
 with Colorizer('red'):
     print('printed in red')
+
+with Colorizer('green'):
+    print('printed in green')
+
+with Colorizer('blue'):
+    print('printed in blue')
+
+with Colorizer('yellow'):
+    print('printed in yellow')
 
 print('printed in default color')
 
--- Homework_6/frange.py
+++ Homework_6/frange.py
@@ -5,34 +5,40 @@ class Frange:
         self.start = start
         self.stop = stop
         self.step = step
+        self.current = start
 
     def __iter__(self):
-        if self.step > 0:
-            current = self.start
-            while current < self.stop:
-                yield current
-                current += self.step
-        elif self.step < 0:
-            current = self.start
-            while current > self.stop:
-                yield current
-                current += self.step
+        return self
+
+    def __next__(self):
+        if self.step > 0 and self.current < self.stop:
+            result = self.current
+            self.current += self.step
+            return result
+        elif self.step < 0 and self.current > self.stop:
+            result = self.current
+            self.current += self.step
+            return result
+        else:
+            raise StopIteration
 
     def __reversed__(self):
         current = self.stop - self.step
+        reversed_list = []
         while current >= self.start:
-            yield current
+            reversed_list.append(current)
             current -= self.step
+        return iter(reversed_list)
 
 
-assert(list(Frange(5)) == [0, 1, 2, 3, 4])
-assert(list(Frange(2, 5)) == [2, 3, 4])
-assert(list(Frange(2, 10, 2)) == [2, 4, 6, 8])
-assert(list(Frange(10, 2, -2)) == [10, 8, 6, 4])
-assert(list(Frange(2, 5.5, 1.5)) == [2, 3.5, 5])
-assert(list(Frange(1, 5)) == [1, 2, 3, 4])
-assert(list(Frange(0, 5)) == [0, 1, 2, 3, 4])
-assert(list(Frange(0, 0)) == [])
-assert(list(Frange(100, 0)) == [])
+assert list(Frange(5)) == [0, 1, 2, 3, 4]
+assert list(Frange(2, 5)) == [2, 3, 4]
+assert list(Frange(2, 10, 2)) == [2, 4, 6, 8]
+assert list(Frange(10, 2, -2)) == [10, 8, 6, 4]
+assert list(Frange(2, 5.5, 1.5)) == [2, 3.5, 5]
+assert list(Frange(1, 5)) == [1, 2, 3, 4]
+assert list(Frange(0, 5)) == [0, 1, 2, 3, 4]
+assert list(Frange(0, 0)) == []
+assert list(Frange(100, 0)) == []
 
 print('SUCCESS!')
